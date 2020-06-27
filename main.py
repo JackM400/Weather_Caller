63afdd0dc1546445d0d4ce653822944916abd8de
Jack Millar <jack.millar400@gmail.com>
2020-06-27 14:04:13 +0100

    print in "pretty" json format , process each element function populated

--- main.py
+++ main.py
@@ -18,18 +18,42 @@ def get_targets():
 
 
 def get_data(location, weather_api_key):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(location, weather_api_key)
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, weather_api_key)
     url_result = requests.get(url)
     return url_result.json()
 
+
+def target_api_calls(targets, weather_api_key):
+    results = {}
+    for location in targets:
+        results[location] = get_data(location, weather_api_key)
+    return results
+
+
+def print_json_pretty(data):
+    print("JSON DATA :\n{}".format(json.dumps(data, indent=4)))
+
+
+def process_json(json_data):
+    result = {}
+    for element in json_data:
+        print("LOCATION  : {}".format(element))
+        result[element] = process_json_element(json_data[element])
+    return result
+
+
+def process_json_element(json_element):
 
 
 def main():
     weather_api_key = get_api_key()
     targets = get_targets()
-    data = []
-    for element in data:
-        print(data[element])
+    json_data = target_api_calls(targets, weather_api_key)
+
+    target_elements = process_json(json_data)
+
+    for target_element in target_elements:
+        print("{} : {}".format(target_element.ljust(15), target_elements[target_element]))
 
 
 main()
